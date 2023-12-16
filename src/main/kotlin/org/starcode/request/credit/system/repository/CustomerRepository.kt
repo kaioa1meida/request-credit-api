b5eb8d626d7007d7e4d59ffd93e6d53ca5524525
kaioa1meida <kaiohsilva04@gmail.com>
2023-12-16 17:21:28 -0300

    create persistency layer

---
+++ src/main/kotlin/org/starcode/request/credit/system/repository/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package org.starcode.request.credit.system.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import org.starcode.request.credit.system.entities.Customer
+
+@Repository
+interface CustomerRepository:JpaRepository<Customer, Long> {
+}
