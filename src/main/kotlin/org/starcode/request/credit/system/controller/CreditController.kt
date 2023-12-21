763966200f9613d86538e1ed6f36bc2a7f0a1dc7
kaioa1meida <kaiohsilva04@gmail.com>
2023-12-21 12:55:53 -0300

    Sending - Controller layer

---
+++ src/main/kotlin/org/starcode/request/credit/system/controller/CreditController.kt
@@ -0,0 +1,54 @@
+package org.starcode.request.credit.system.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.starcode.request.credit.system.dto.request.CreditDto
+import org.starcode.request.credit.system.dto.response.CreditView
+import org.starcode.request.credit.system.dto.response.CreditViewList
+import org.starcode.request.credit.system.entities.Credit
+import org.starcode.request.credit.system.service.implementation.CreditService
+import java.util.UUID
+import java.util.stream.Collectors
+
+@RestController
+@RequestMapping("/api/credit/")
+class CreditController(
+    private val creditService: CreditService
+) {
+
+
+    @PostMapping
+    fun saveCredit(@RequestBody creditDto: CreditDto): ResponseEntity<String> {
+        val credit: Credit = this.creditService.saveCredit(creditDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body("Credit ${credit.creditCode}, by customer: ${credit.customer?.firstName} created!")
+    }
+
+    @GetMapping
+    fun findCreditByCustomer(
+        @RequestParam(value = "customerId") customerId: Long
+    ): ResponseEntity<List<CreditViewList>> {
+        val creditViewList: List<CreditViewList> = this.creditService.findAllCreditsByCustomerId(customerId)
+            .stream()
+            .map { credit: Credit -> CreditViewList(credit) }
+            .collect(Collectors.toList())
+
+        return ResponseEntity.status(HttpStatus.OK).body(creditViewList)
+    }
+
+    @GetMapping("{creditCode}")
+    fun findCreditByCreditCode(
+        @PathVariable creditCode: UUID,
+        @RequestParam(value = "customerId") customerId: Long
+    ): ResponseEntity<CreditView> {
+        val credit: Credit = this.creditService.findCreditByCreditCode(customerId, creditCode)
+        return ResponseEntity.status(HttpStatus.OK).body(CreditView(credit))
+    }
+}
---
+++ src/main/kotlin/org/starcode/request/credit/system/controller/CustomerController.kt
@@ -0,0 +1,47 @@
+package org.starcode.request.credit.system.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.starcode.request.credit.system.dto.request.CustomerDto
+import org.starcode.request.credit.system.dto.request.CustomerUpdateDto
+import org.starcode.request.credit.system.dto.response.CustomerView
+import org.starcode.request.credit.system.entities.Customer
+import org.starcode.request.credit.system.service.implementation.CustomerService
+
+@RestController
+@RequestMapping("/api/customer/")
+class CustomerController(
+    private val customerService: CustomerService
+) {
+
+    @GetMapping("{id}")
+    fun findCustomer(@PathVariable id: Long): ResponseEntity<CustomerView> {
+        val customer: Customer = this.customerService.findCustomer(id)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customer))
+    }
+
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<String> {
+        val savedUser = customerService.saveCustomer(customerDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED).body("Customer: ${savedUser.email} saved!")
+    }
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): ResponseEntity<CustomerView> {
+        val customer: Customer = this.customerService.findCustomer(id)
+        val customerToUpdate: Customer = customerUpdateDto.toEntity(customer)
+        val customerUpdated: Customer = this.customerService.saveCustomer(customerToUpdate)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customerUpdated))
+    }
+
+    @DeleteMapping("{id}")
+    fun deleteCustomer(@PathVariable id: Long): ResponseEntity<String> {
+        this.customerService.deleteCustomer(id)
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Customer deleted!")
+    }
+}
